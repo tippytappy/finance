d51f2006da061c72d844c32fa9d872f115f76745
tippytappy <dylanomran2@gmail.com>
2020-07-07 06:50:53 +0100

    Added a basic multi-symbol plot
    The plot uses ggplot to display close
    price and simple moving averages (50
    and 200 day) for multiple symbols.

--- tidyquant.R
+++ tidyquant.R
@@ -1,6 +1,6 @@
 library(pacman)
 p_load(tidyverse, tidyquant, timetk, tsibble, scales, highcharter, 
-       broom, PerformanceAnalytics, tibbletime)
+       broom, PerformanceAnalytics, tibbletime, ggplot2)
 
 # GET DATA  ###################################################################
 prices <- tq_get(c('MPNGF', 'JD', 'PDD', 'ITM'), 
@@ -15,3 +15,15 @@ prices <- prices %>%
   group_by(symbol) %>% 
   mutate(Ra_50 = sma_50(close),
          Ra_200 = sma_200(close))
+
+# MOVING AVERAGES PLOTS  ######################################################
+prices %>% 
+  #filter(symbol == 'MPNGF') %>% 
+  ggplot() +
+  geom_line(aes(date, close), alpha = 0.3) + 
+  geom_line(aes(date, Ra_50), color = 'blue', lwd = 1) + 
+  geom_line(aes(date, Ra_200), color = 'red') + 
+  facet_wrap(~ symbol, scales = 'free') + 
+  theme_light() + 
+  labs(title = "Martin's Stocks", 
+       subtitle = 'Close price with 50 (blue) and 200 (red) simple moving averages')
