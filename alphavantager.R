7c68dee147137d9e6eebc623fdf759aee7a3e610
tippytappy <dylanomran2@gmail.com>
2020-07-27 15:11:57 +0100

    Added a function to get company overviews
    Also shows how to use purrr::map to get data for multiple companies.

--- alphavantager.R
+++ alphavantager.R
@@ -1,12 +1,31 @@
+# SET UP  #####################################################################
 library(alphavantager)
 library(jsonlite)
+library(dplyr)
 
 # set the api key
 av_key <- av_api_key(Sys.getenv('alphavantage_key'))
 
-# get time series data
+
+# GET TIME SERIES DATA  #######################################################
+# get time series data for 1 symbol
 av_get('AAPL', 'TIME_SERIES_DATA')
 
+# COMPANY OVERVIEWS  ##########################################################
+# function to get company overview
+get_overview <- function(symbol) {
+  av_overview <- fromJSON(paste0('https://www.alphavantage.co/query?function=OVERVIEW&symbol=', 
+                             symbol, '&apikey=', av_key))
+  data.frame(t(sapply(av_overview,c)))
+}
+
+# get multiple overviews
+company_overviews <- map(c('MSFT', 'AAPL'), ~ get_overview(.x)) %>% 
+  bind_rows()
+get_overview('MSFT')
+
+
+# FUNDAMENTALS  ###############################################################
 # function to extract fundamentals
 get_fundamentals <- function(symbol, info_type = 'income', report_type = 'annual') {
   url_start <- 'https://www.alphavantage.co/query?function='
@@ -32,14 +51,26 @@ get_fundamentals <- function(symbol, info_type = 'income', report_type = 'annual
 
 # use map to get fundamentals for more than one stock
 library(purrr)
-company_fundamentals <- map(c('AAPL', 'MSFT', 'AMZN', 'GOOG'), ~ get_fundamentals(.x)) %>% 
+income_statements <- map(c('AAPL', 'MSFT', 'AMZN', 'GOOG'), 
+                                ~ get_fundamentals(.x, 'income')) %>% 
   bind_rows()
 
+balance_sheets <- map(c('AAPL', 'MSFT', 'AMZN', 'GOOG'), 
+                                ~ get_fundamentals(.x, 'balance')) %>% 
+  bind_rows()
+
+cashflow_statements <- map(c('AAPL', 'MSFT', 'AMZN', 'GOOG'), 
+                                 ~ get_fundamentals(.x, 'cash')) %>% 
+  bind_rows()
+
+
+# PLOT THE DATA  ##############################################################
 # plot some data
 library(ggplot2)
-company_fundamentals %>% 
+company_fundamentals_inc %>% 
   ggplot(aes(as.POSIXct(fiscalDateEnding), as.numeric(incomeTaxExpense))) + 
   geom_line() +
   facet_wrap(~ symbol) + 
   labs(x = '') + 
   theme_light()
+
