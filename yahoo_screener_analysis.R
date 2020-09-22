3615d0e6cafd05c7998d7f32a9368a6f639c9f23
tippytappy <dylanomran2@gmail.com>
2020-09-22 21:47:50 +0100

    Changed the function to return symbols not data.
    This lets the user choose between tidyquant, quantmod etc. for downloading the data. Also removed the custom screeners because they expire after a few days so the urls become useless.

--- yahoo_screener_analysis.R
+++ yahoo_screener_analysis.R
@@ -1,7 +1,7 @@
 # SET UP  #####################################################################
 library(rvest)
 
-#  GET THE DATA  ##############################################################
+# PRE-DEFINED SCREENERS  ######################################################
 # yahoo's pre-defined screeners
 screeners <- c(
   'aggressive_small_caps',
@@ -19,19 +19,17 @@ screeners <- c(
   'undervalued_large_caps')
 
 
-get_screener_data <- function(screen_num = 12, num_records = 200, ...) {
+get_screeners_symbols <- function(screen_num = 12, num_records = 200, ...) {
   screener_url <- paste0('https://uk.finance.yahoo.com/screener/predefined/',
                             screeners[screen_num],
                          '?offset=0&count=',
                          num_records)
   cat(paste0('getting symbols from \n', screener_url))
-  screener_symbols <- 
-    read_html(screener_url) %>% 
+  read_html(screener_url) %>% 
     rvest::html_nodes('.simpTblRow') %>% 
     rvest::html_nodes("a") %>% 
-    rvest::html_text() %>%
-    tq_get()
+    rvest::html_text()
 }
 
 # example: get 30 solid mid-cap growth funds
-screener_data <- get_screener_data(10, 30)
+screener_symbols <- get_screeners_data(10, 30)
